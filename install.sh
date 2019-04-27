3c526956bddf16692e04a062d389e2496e97421b
Dragon8oy <stuart.a.hayhurst@gmail.com>
2019-04-27 16:56:28 +0100

    Improved installer and updating behaviour, added notifier for updates

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR
+sudo apt install git -y
+
 #Move kernel-notify to /usr/bin/kernel-notify
 sudo cp kernel-notify /usr/bin/kernel-notify
 
@@ -11,11 +13,23 @@ if [ -d "/usr/share/kernel-notify" ]; then
 else
   echo "/usr/share/kernel-notify not found, creating it"
   sudo mkdir /usr/share/kernel-notify
+  echo "Created directory"
 fi
+
 sudo cp icon.png /usr/share/kernel-notify/icon.png
+echo "Added icon"
 
 #Move kernel-notify.desktop to ~/.config/autostart/ and /usr/share/kernel-notify/
 mv kernel-notify.desktop ~/.config/autostart/kernel-notify.desktop
 sudo cp ~/.config/autostart/kernel-notify.desktop /usr/share/kernel-notify/kernel-notify.desktop
+echo "Added autostart file"
 
-cd ../ && rm -rf $DIR
+cd /usr/share/kernel-notify
+if [ -d "/usr/share/kernel-notify" ]; then
+  echo "Git repo already cloned"
+else
+  sudo git clone https://github.com/Dragon8oy/kernel-notify.git
+  echo "Clone git repo"
+fi
+
+rm -rf $DIR
