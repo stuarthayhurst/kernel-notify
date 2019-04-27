d58a2f8ec69c895e7e9491e60a0ef28698d9c6d5
Dragon8oy <stuart.a.hayhurst@gmail.com>
2019-04-27 14:23:16 +0100

    Only made /usr/share/kernel-notify if needed and remove the directory installing from

--- install.sh
+++ install.sh
@@ -3,13 +3,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR
 #Move kernel-notify to /usr/bin/kernel-notify
-sudo mv kernel-notify /usr/bin/kernel-notify
+sudo cp kernel-notify /usr/bin/kernel-notify
 
 #Make /usr/share/kernel-notify and move icon.png to /usr/share/kernel-notify/icon.png
-sudo mkdir /usr/share/kernel-notify
-sudo mv icon.png /usr/share/kernel-notify/icon.png
+if [ -d "/usr/share/kernel-notify" ]; then
+  echo "/usr/share/kernel-notify was found, not creating it"
+else
+  echo "/usr/share/kernel-notify not found, creating it"
+  sudo mkdir /usr/share/kernel-notify
+fi
+sudo cp icon.png /usr/share/kernel-notify/icon.png
 
-#Move kernel-notify.desktop to ~/.config/autostart/kernel-notify.desktop
-cp kernel-notify.desktop ~/.config/autostart/kernel-notify.desktop
-sudo cp kernel-notify.desktop /usr/share/kernel-notify/kernel-notify.desktop
-sudo rm kernel-notify.desktop
+#Move kernel-notify.desktop to ~/.config/autostart/ and /usr/share/kernel-notify/
+mv kernel-notify.desktop ~/.config/autostart/kernel-notify.desktop
+sudo cp ~/.config/autostart/kernel-notify.desktop /usr/share/kernel-notify/kernel-notify.desktop
+
+cd ../ && rm -rf $DIR
