3b936b4c4ac188dd1169eb9d2428773ae573c9ef
Dragon8oy <stuart.a.hayhurst@gmail.com>
2019-08-28 16:23:12 +0100

    Fixed icon pixels missing

--- notifications.cc
+++ notifications.cc
@@ -1,18 +1,23 @@
 #include <libnotify/notify.h>
 #include <iostream>
 
+int my_callback_func ()
+{
+  std::cout << "Test" << std::endl;
+  return 0;
+}
+
 int main(int argc, char * argv[] ) 
 {
     GError *error = NULL;
-    GdkPixbuf *icon;
-    icon = gdk_pixbuf_new_from_file(argv[3], &error);
     notify_init("Basics");
     NotifyNotification* n = notify_notification_new (argv[1], 
                                  argv[2],
-                                  0);
-    notify_notification_set_icon_from_pixbuf
-                                                            (n,
-                                                              icon);
+                                 argv[3]);
+    //notify_notification_add_action (n,
+    //    const char *action_click,
+    //    const char *label,
+    //    NotifyActionCallback my_callback_func);
     notify_notification_set_timeout(n, 10000);
 
     if (!notify_notification_show(n, 0)) 
