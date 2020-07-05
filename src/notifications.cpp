c1c49fc8b667655989d6cca5e210cda7fe8e196c
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2020-07-05 17:53:45 +0100

    Remove commented out code

--- src/notifications.cpp
+++ src/notifications.cpp
@@ -3,24 +3,6 @@
 
 int action_triggered = 0;
 
-//void callback_update_program(NotifyNotification* n, char* action, gpointer user_data) {
-//  std::cout << "Updating Program" << std::endl;
-//  action_triggered = 1;
-//  system("workDir=$(pwd) $(pwd)/actions --display");
-//  system("pkexec kernel-notify -au");
-//}
-//void callback_update_kernel(NotifyNotification* n, char* action, gpointer user_data) {
-//  std::cout << "Updating Kernel" << std::endl;
-//  action_triggered = 1;
-//  system("workDir=$(pwd) $(pwd)/actions --display");
-//  system("pkexec kernel-notify -aa");
-//}
-//void callback_mute(NotifyNotification* n, char* action, gpointer user_data) {
-//  std::cout << "Muting Program" << std::endl;
-//  action_triggered = 1;
-//  system("pkexec kernel-notify -am");
-//}
-
 void callback_update_program() {
   std::cout << "Updating Program" << std::endl;
   action_triggered = 1;
