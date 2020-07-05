59f154cbff3f5f428c845c787510b7c69bc7bdd6
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2020-07-05 19:11:26 +0100

    Added new build system using make

---
+++ Makefile
@@ -0,0 +1,108 @@
+CC=g++
+CFLAGS:=-Wall -Wextra $(shell pkg-config --cflags libnotify)
+LDFLAGS:=$(shell pkg-config --libs libnotify)
+SHELL=bash
+
+BUILD_DIR=build
+DIST_DIR=dist
+
+UNINSTALL_LIST:=$(shell cat src/uninstall.list)
+ICON_RESOLUTIONS=48 64 128 256
+
+.PHONY: build install uninstall dist debian notifications icons docs check test clean
+
+build:
+	mkdir -p $(BUILD_DIR)
+	sed "s|.*Exec=.*|Exec=kernel-notify -zw|" src/kernel-notify.desktop > build/kernel-notify.desktop
+	make notifications
+	make docs
+install:
+	mkdir -p "/usr/share/kernel-notify"
+	if [[ -f "/usr/share/kernel-notify/config" ]]; then \
+	  mv /usr/share/kernel-notify/config /usr/share/kernel-notify/config.old; \
+	fi
+	if [[ -f "/etc/xdg/autostart/kernel-notify.desktop" ]]; then \
+	  mv /etc/xdg/autostart/kernel-notify.desktop /usr/share/kernel-notify/kernel-notify.desktop.old; \
+	fi
+	while IFS='' read -r line || [ -n "$$line" ]; do \
+	  if [[ "$$line" = *"+x"* ]]; then \
+	    makeExecutable="true"; \
+	    line="$${line// +x}"; \
+	  fi; \
+	  cp -v $$line; \
+	  if [[ "$$makeExecutable" == "true" ]]; then \
+	    chmod +x "$${line#* }"; \
+	    makeExecutable="false"; \
+	  fi; \
+	done < src/install.list
+	./install.sh --make-assist
+uninstall:
+	rm -rf $(UNINSTALL_LIST)
+dist:
+	make debian
+debian:
+	make build
+	mkdir -p $(DIST_DIR)
+	cp -r src/debian $(DIST_DIR)
+	for filelist in src/install.list src/debian.list; do \
+	  while IFS='' read -r line || [ -n "$$line" ]; do \
+	    if [[ "$$line" = *"+x"* ]]; then \
+	      makeExecutable="true"; \
+	      line="$${line// +x}"; \
+	    fi; \
+	    file="$${line% *}"; \
+	    dir="$${line#* }"; \
+	    dir="$(DIST_DIR)/debian$${dir%/*}/"; \
+	    mkdir -p "$$dir"; \
+	    cp "$$file" "$$dir"; \
+	    if [[ "$$makeExecutable" == "true" ]]; then \
+	      chmod +x "$(DIST_DIR)/debian$${line#* }"; \
+	      makeExecutable="false"; \
+	    fi; \
+	  done < "$$filelist"; \
+	done
+	uid="$$(ls -nd $(DIST_DIR)/debian/ |cut -d' ' -f3)"; \
+	gid="$$(ls -nd $(DIST_DIR)/debian/ |cut -d' ' -f4)"; \
+	version="$$(sed -n '5p' $(BUILD_DIR)/kernel-notify.desktop)"; \
+	version="$${version//Version=}"; \
+	sudo chown -R root:root $(DIST_DIR)/debian/; \
+	sudo dpkg --build $(DIST_DIR)/debian/ $(DIST_DIR)/kernel-notify-"$$version"_all.deb; \
+	sudo chown -R "$$uid:$$gid" $(DIST_DIR)/debian/ $(DIST_DIR)/kernel-notify-"$$version"_all.deb
+notifications:
+	mkdir -p $(BUILD_DIR)
+	$(CC) src/notifications.cpp -o $(BUILD_DIR)/notifications $(CFLAGS) $(LDFLAGS);
+icons:
+	for resolution in $(ICON_RESOLUTIONS); do \
+	  mkdir -p "./icons/"$$resolution"x"$$resolution; \
+	  mkdir -p "./icons/"$$resolution"x"$$resolution"/apps"; \
+	  for filename in ./icons/*.svg; do \
+	    inkscape --export-filename=$${filename//.svg/.png} -w $$resolution -h $$resolution $$filename; \
+	    mv $${filename//.svg/.png} "./icons/"$$resolution"x"$$resolution"/apps"; \
+	  done; \
+	done
+	for resolution in $(ICON_RESOLUTIONS); do \
+	  for filename in "./icons/"$$resolution"x"$$resolution"/apps/*.png"; do \
+	    optipng -strip all $$filename; \
+	  done; \
+	done
+	cp ./icons/256x256/apps/kernel-notify.png ./docs/icon.png
+docs:
+	mkdir -p $(BUILD_DIR)
+	gzip -cqv9 docs/kernel-notify.1 > $(BUILD_DIR)/kernel-notify.1.gz
+check:
+	make test
+test:
+	while IFS='' read -r line || [ -n "$$line" ]; do \
+	  if [[ "$$line" = *"+x"* ]]; then \
+	    line="$${line// +x}"; \
+	  fi; \
+	  if [[ ! -f "$${line% *}" ]]; then \
+	    echo "$${line% *} is missing. Have you run 'make build'?"; \
+	    failed="true"; \
+	  fi; \
+	done < src/install.list; \
+	if [[ "$$failed" == "true" ]]; then \
+	  exit 1; \
+	fi
+clean:
+	rm -rf $(BUILD_DIR) $(DIST_DIR)
--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
-source functions
 
 uninstall() {
   if [[ "$USER" != "root" ]]; then
@@ -9,88 +7,18 @@ uninstall() {
     read -r -p "Are you sure you want to uninstall? [y/N] " response
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
       echo "Uninstalling:"
-      if dpkg -s kernel-notify | grep Status |grep -q installed; then
-        echo "Kernel-notify installed via .deb, removing"
-        checkDpkg
-        dpkg -r kernel-notify
-        exit
-      else
-        uninstallList=$(cat "./uninstall-list")
-        for filename in $uninstallList; do
-          if [ -f "$filename" ] || [ -d "$filename" ]; then
-            rm -rfv "$filename"
-          fi
-        done
-        echo "Done"; exit
-      fi
+      sudo make uninstall
     fi
   fi
-}
-
-buildNotifications() {
-  if ! g++ --version > /dev/null 2>&1; then
-    echo "g++ not found, required to build notifications"
-    echo "  ATTENTION: g++: failed to build notifications"
-  elif ! ls /usr/include/libnotify/notify.h > /dev/null 2>&1; then
-    echo "libnotify-dev not found, required to build notifications"
-    echo "  ATTENTION: g++: failed to build notifications"
-  elif g++ notifications.cpp -o notifications `pkg-config --cflags --libs libnotify`; then
-    echo "g++: built notifications"
-  else
-    echo "  ATTENTION: g++: failed to build notifications"
-  fi
-}
-
-compressIcons() {
-  echo "Generating and compressing icons:"
-  if [[ "$slow" == "true" ]]; then
-    optipngArgs="-o7 -zm1-9"
-  else
-    optipngArgs=""
-  fi
-
-  for resolution in ${outputResolutions[@]}; do
-    for filename in ./icons/*.svg; do
-      inkscape --export-filename="${filename//.svg}.png" -w "$resolution" -h "$resolution" "$filename"
-      if [ ! -d "./icons/${resolution}x${resolution}" ]; then
-        mkdir "./icons/${resolution}x${resolution}"
-        mkdir "./icons/${resolution}x${resolution}/apps"
-      fi
-      mv "${filename//.svg}.png" "./icons/${resolution}x${resolution}/apps/"
-    done
-  done
-
-  for resolution in ${outputResolutions[@]}; do
-    for filename in ./icons/${resolution}x${resolution}/apps/*.png; do
-      optipng $optipngArgs -strip all "$filename"
-    done
-  done
-  if [ -f "./icons/256x256/apps/kernel-notify.png" ]; then
-    cp ./icons/256x256/apps/kernel-notify.png ./docs/icon.png
-  fi
-}
-
-cleanFiles() {
-  if [ -f "./notifications" ]; then
-    rm -v notifications 
-  fi
-  if [ -f "kernel-notify-"*"_all.deb" ]; then
-    rm -v kernel-notify-*_all.deb
-  fi
-  for resolution in ${outputResolutions[@]}; do
-    if [ -d "./icons/${resolution}x${resolution}" ]; then
-      rm -rfv "./icons/${resolution}x${resolution}"
-    fi
-  done
 }
 
 checkDeps() {
   if [[ "$1" == *"p"* ]]; then
     echo "-------------------------------"; echo ""
 
-    deps=("awk curl cmp file git grep less pkexec sed sudo")
-    for i in $deps; do
-      if which $i > /dev/null 2>&1; then
+    deps=("awk" "curl" "cmp" "file" "git" "grep" "less" "pkexec" "sed" "sudo")
+    for i in "${deps[@]}"; do
+      if which "$i" > /dev/null 2>&1; then
         echo "${i^} found"
       else
         echo "${i^} not found"
@@ -120,10 +48,14 @@ checkDeps() {
     echo ""; echo "-------------------------------"; echo ""
   fi
 
-  if [[ "$1" == *"b"* ]]; then
-    buildDeps=("g++ inkscape optipng pkg-config sed")
-    for i in $buildDeps; do
-      if which $i > /dev/null 2>&1; then
+  if [[ "$1" == *"b"* ]] || [[ "$1" == *"i"* ]]; then
+    if [[ "$1" == *"i"* ]]; then
+      buildDeps=("g++" "inkscape" "optipng" "pkg-config" "sed")
+    else
+      buildDeps=("g++" "pkg-config" "sed")
+    fi
+    for i in "${buildDeps[@]}"; do
+      if which "$i" > /dev/null 2>&1; then
         echo "${i^} found"
       else
         echo "${i^} not found"
@@ -157,18 +89,20 @@ checkDeps() {
     fi
 }
 
-buildPackage() {
+updateVersion() {
   if [[ "$1" != "" ]]; then
     newVersion="$1"
     echo "Building package v$buildVersion:"
   else
-    echo "Enter the new version number: (Leave blank to only build package)"
-    read newVersion
+    echo "Enter the new version number:"
+    read -r newVersion
   fi
-  compressIcons
   if [[ "$newVersion" == "" ]]; then
-    newVersion=$(cat kernel-notify.desktop | sed -n '5p')
-    newVersion=${newVersion//Version=}
+    echo "No new version entered, exiting"
+    exit
+  fi
+
+  if [[ "$1" == "" ]]; then
     echo "Building package:"
   fi
   read -r -p "Update manpage date? [y/N] " response
@@ -177,217 +111,84 @@ buildPackage() {
     sed "s|Built: .*|Built: $manDate\" \"Version: $newVersion\" \"kernel-notify man page\"|" docs/kernel-notify.1 > docs/kernel-notify.1.temp
     mv -v docs/kernel-notify.1.temp docs/kernel-notify.1
   fi
-  sed "s|.*version=\".*|version=\"$newVersion\"|" kernel-notify > kernel-notify.temp
-  mv -v kernel-notify.temp kernel-notify
-  sed "s|.*Version=.*|Version=$newVersion|" kernel-notify.desktop > kernel-notify.desktop.temp
-  mv -v kernel-notify.desktop.temp kernel-notify.desktop
-
-  sed "s|.*Version:.*|Version: $newVersion|" package/debian/DEBIAN/control > package/debian/DEBIAN/control.temp
-  mv -v package/debian/DEBIAN/control.temp package/debian/DEBIAN/control
-
-  chmod -v +x actions
-  chmod -v +x updater
-  chmod -v +x kernel-notify
-
-  if which dpkg > /dev/null 2>&1; then
-    debianPath="package/debian/usr/share/kernel-notify"
-    iconPath="package/debian/usr/share/icons/hicolor"
-    mkdir -v package/debian/usr && mkdir -v package/debian/usr/share && mkdir -v package/debian/usr/share/kernel-notify && mkdir -v package/debian/usr/share/applications && mkdir -v package/debian/usr/share/man && mkdir -v package/debian/usr/share/man/man1
-    mkdir -v package/debian/usr/bin
-    mkdir -v package/debian/etc && mkdir -v package/debian/etc/xdg && mkdir -v package/debian/etc/xdg/autostart
-    mkdir -v package/debian/etc/bash_completion.d/
-
-    mkdir -v package/debian/usr/share/icons && mkdir -v package/debian/usr/share/icons/hicolor && mkdir -v package/debian/usr/share/icons/hicolor/scalable && mkdir -v package/debian/usr/share/icons/hicolor/scalable/apps
-    for resolution in ${outputResolutions[@]}; do
-      mkdir -v "package/debian/usr/share/icons/hicolor/${resolution}x${resolution}"
-      mkdir -v "package/debian/usr/share/icons/hicolor/${resolution}x${resolution}/apps"
-    done
-
-    buildNotifications
-    chmod +x notifications
-    gzip -kqv9 docs/kernel-notify.1 docs/kernel-notify.1.gz
-
-    cp -v actions $debianPath/
-    cp -v config $debianPath/
-    cp -v docs/kernel-notify.1.gz package/debian/usr/share/man/man1/
-    cp -v functions $debianPath/
-    cp -v kernel-notify.desktop package/debian/etc/xdg/autostart/
-    cp -v kernel-notify.desktop package/debian/usr/share/applications/
-    cp -v kernel-notify package/debian/usr/bin/
-    cp -v kernel-notify-prompt package/debian/etc/bash_completion.d/
-    cp -v notifications $debianPath/
-    cp -v notifications.cpp $debianPath/
-    cp -v updater $debianPath/
-
-    for resolution in ${outputResolutions[@]}; do
-      for filename in ./icons/${resolution}x${resolution}/apps/*.png; do
-        cp -v "$filename" "$iconPath/${resolution}x${resolution}/apps/"
-      done
-    done
-    cp -v ./icons/kernel-notify.svg $iconPath/scalable/apps/kernel-notify.svg
-    cp -v ./icons/kernel-notify-app.svg $iconPath/scalable/apps/kernel-notify-app.svg
-
-    sed "s|.*Exec=.*|Exec=kernel-notify -zw|" package/debian/usr/share/applications/kernel-notify.desktop > package/debian/usr/share/applications/kernel-notify.desktop.temp
-    mv -v package/debian/usr/share/applications/kernel-notify.desktop.temp package/debian/usr/share/applications/kernel-notify.desktop
-
-    uid="$(ls -nd package/debian |cut -d' ' -f3)"
-    gid="$(ls -nd package/debian |cut -d' ' -f4)"
-    sudo chown -R root:root package/debian/
-    sudo dpkg --build package/debian/ ./kernel-notify-"$newVersion"_all.deb
-    sudo chown -R "$uid:$gid" package/debian/ ./kernel-notify-"$newVersion"_all.deb
-
-    rm -v notifications
-    rm -v docs/kernel-notify.1.gz
-    rm -rfv package/debian/etc/
-    rm -rfv package/debian/usr/
-    echo "Done"
-  else
-    echo "Building Debian packages not supported on this system"
-    exit
-  fi
+  sed "s|.*version=\".*|version=\"$newVersion\"|" src/kernel-notify > src/kernel-notify.temp
+  mv -v src/kernel-notify.temp src/kernel-notify
+  chmod +x src/kernel-notify
+  sed "s|.*Version=.*|Version=$newVersion|" src/kernel-notify.desktop > src/kernel-notify.desktop.temp
+  mv -v src/kernel-notify.desktop.temp src/kernel-notify.desktop
+  sed "s|.*Version:.*|Version: $newVersion|" src/debian/DEBIAN/control > src/debian/DEBIAN/control.temp
+  mv -v src/debian/DEBIAN/control.temp src/debian/DEBIAN/control
 }
 
-installPackage() {
-  if [[ "$USER" != "root" ]]; then
-    echo "  ATTENTION: Insufficient permission, please rerun with root"
-  else
-    cp kernel-notify /usr/bin/kernel-notify
-    if [ -d "/usr/share/kernel-notify" ]; then
-      echo "/usr/share/kernel-notify found"
-      mv /usr/share/kernel-notify/config /usr/share/kernel-notify/config.old
-    else
-      echo "/usr/share/kernel-notify not found, creating it"
-      mkdir /usr/share/kernel-notify
-      echo "Created program directory"
-    fi
+installDebian() {
+  make dist
+  currVersion="$(sed -n '5p' "src/kernel-notify.desktop")"
+  currVersion=${currVersion//Version=}
+  sudo dpkg -i "./dist/kernel-notify-${currVersion}_all.deb"
+  make clean
+}
 
-    buildNotifications
-    
-    if [ -f /etc/xdg/autostart/kernel-notify.desktop ]; then
-      echo "Existing autostart file found, saving"
-      cp /etc/xdg/autostart/kernel-notify.desktop /usr/share/kernel-notify/kernel-notify.desktop.old
+makeAssist() {
+  if [[ -f "/usr/share/kernel-notify/kernel-notify.desktop.old" ]]; then
+    autostartEnabled="$(grep "X-GNOME-Autostart-enabled=" "/usr/share/kernel-notify/kernel-notify.desktop.old" |grep -v "#")"
+    autostartEnabled="${autostartEnabled//X-GNOME-Autostart-enabled=}"
+    if [ "$autostartEnabled" == "false" ]; then
+      autostartFile="/etc/xdg/autostart/kernel-notify.desktop"
+      sed "s|X-GNOME-Autostart-enabled=true|X-GNOME-Autostart-enabled=false|" "$autostartFile" > "/tmp/kernel-notify-autostart-debian.desktop.temp"
+      mv "/tmp/kernel-notify-autostart-debian.desktop.temp" "$autostartFile"
     fi
-    if [ -d "/etc/xdg/autostart" ]; then
-      echo "Installing autostart file..."
-      cp -v kernel-notify.desktop /etc/xdg/autostart/kernel-notify.desktop
-    fi
-    if [[ -f "/usr/share/kernel-notify/kernel-notify.desktop.old" ]]; then
-      autostartEnabled="$(cat "/usr/share/kernel-notify/kernel-notify.desktop.old" |grep "X-GNOME-Autostart-enabled=" |grep -v "#")"
-      autostartEnabled="${autostartEnabled//X-GNOME-Autostart-enabled=}"
-      if [ "$autostartEnabled" == "false" ]; then
-        autostartFile="/etc/xdg/autostart/kernel-notify.desktop"
-        sed "s|X-GNOME-Autostart-enabled=true|X-GNOME-Autostart-enabled=false|" "$autostartFile" > "/tmp/kernel-notify-autostart-debian.desktop.temp"
-        mv "/tmp/kernel-notify-autostart-debian.desktop.temp" "$autostartFile"
-      fi
-    fi
-
-    chmod +x kernel-notify
-    chmod +x actions
-    chmod +x updater
-
-    if [ -f "./notifications" ]; then
-      echo "Installing notifications..."
-      chmod +x notifications
-      mv -v notifications /usr/share/kernel-notify/notifications
-      mv -v notifications.cpp /usr/share/kernel-notify/notifications.cpp
-    fi
-
-    if [ -d "/usr/share/man/man1/" ]; then
-      echo "Installing manpages..."
-      gzip -kqv9 docs/kernel-notify.1 docs/kernel-notify.1.gz
-      cp -v docs/kernel-notify.1.gz /usr/share/man/man1/
-      rm -v docs/kernel-notify.1.gz
-    fi
-
-    if [[ -d "/etc/bash_completion.d" ]]; then
-      echo "Installing bash autocompletion..."
-      cp -v kernel-notify-prompt /etc/bash_completion.d/
-    fi
-
-    echo "Installing other program files..."
-    cp -v actions /usr/share/kernel-notify/actions
-    cp -v config /usr/share/kernel-notify/config
-    cp -v functions /usr/share/kernel-notify/functions
-    cp -v updater /usr/share/kernel-notify/updater
-
-    echo "Installing icons..."
-    for resolution in ${outputResolutions[@]}; do
-      for filename in ./icons/${resolution}x${resolution}/apps/*.png; do
-        cp -v "$filename" "/usr/share/icons/hicolor/${resolution}x${resolution}/apps/"
-      done
+  fi
+  if [ -f /usr/share/kernel-notify/config.old ] && ! diff /usr/share/kernel-notify/config /usr/share/kernel-notify/config.old > /dev/null; then
+    echo "Updating config values..."
+    configs=$(grep -v "#" "/usr/share/kernel-notify/config.old" |sed 's|=.*||')
+    for i in $configs; do
+      configValue=$(grep -v "#" "/usr/share/kernel-notify/config.old" |grep "$i" |sed 's|.*=||')
+      configValue=${configValue//'"'}
+      kernel-notify -c "$i" "$configValue" "silent"
     done
-    if [ -d "/usr/share/icons/hicolor/scalable/apps/" ]; then
-      cp -v ./icons/kernel-notify.svg /usr/share/icons/hicolor/scalable/apps/kernel-notify.svg
-      cp -v ./icons/kernel-notify-app.svg /usr/share/icons/hicolor/scalable/apps/kernel-notify-app.svg
-    fi
-
-    echo "Installing desktop entry..."
-    sed "s|.*Exec=.*|Exec=kernel-notify -zw|" kernel-notify.desktop > kernel-notify.desktop.temp
-    mv -v kernel-notify.desktop.temp /usr/share/applications/kernel-notify.desktop
-
-    echo "Installed program files"
-
-    if [ -f /usr/share/kernel-notify/config.old ] && ! diff /usr/share/kernel-notify/config /usr/share/kernel-notify/config.old > /dev/null; then
-      echo "Updating config values..."
-      configs=$(cat /usr/share/kernel-notify/config.old |grep -v "#" |sed 's|=.*||')
-      for i in $configs; do
-        configValue=$(cat /usr/share/kernel-notify/config.old |grep -v "#" |grep "$i" |sed 's|.*=||')
-        configValue=${configValue//'"'}
-        kernel-notify -c "$i" "$configValue" "silent"
-      done
-      echo "  ATTENTION: Config updated, run 'kernel-notify -o' to view the old config"
-    fi
-
+    echo "  ATTENTION: Config updated, run 'kernel-notify -o' to view the old config"
+  fi
     if which update-desktop-database > /dev/null; then
-      echo "Updating desktop-file-utils"; update-desktop-database > /dev/null
-    fi
-    if which update-mime > /dev/null; then
-      echo "Updating mime-support"; update-mime > /dev/null
-    fi
-    if which gtk-update-icon-cache > /dev/null; then
-      echo "Updating hicolor-icon-theme"; touch /usr/share/icons/hicolor > /dev/null
-      gtk-update-icon-cache -f /usr/share/icons/hicolor/ > /dev/null
-    fi
-    if which mandb > /dev/null; then
-      echo "Updating man-db"; mandb 2>&1 > /dev/null
-    fi
-    echo "Done"
-
-    echo ""; echo "-------------------------------"; echo ""
-    if kernel-notify -v; then
-      echo ""; echo "Successfully installed / updated kernel-notify"
-    else
-      echo ""; echo "  ATTENTION: Installing / updating kernel-notify failed"
-    fi
+    echo "Updating desktop-file-utils"; update-desktop-database > /dev/null
+  fi
+  if which gtk-update-icon-cache > /dev/null; then
+    echo "Updating gtk-update-icon-cache"; touch /usr/share/icons/hicolor > /dev/null
+    gtk-update-icon-cache -f /usr/share/icons/hicolor/ &> /dev/null
   fi
 }
 
-if [[ "$@" == *"-s"* ]]; then
-  slow="true"
-fi
-
-for i in $@; do
+for i in "$@"; do
   if [[ "$i" != *"d"* ]] && [[ "$i" != *"s"* ]] && [[ "$i" != *"-"* ]]; then
     buildVersion="$i"
   fi
 done
 
-outputResolutions=(48 64 128 256)
 while [[ "$#" -gt 0 ]]; do case $1 in
-  -h|--help) echo "Kernel-notify Copyright (C) 2020 Stuart Hayhurst"; echo "This program comes with ABSOLUTELY NO WARRANTY."; echo "This is free software; see the source for copying conditions."; echo ""; echo "Usage: ./install.sh [-OPTION]"; echo "Help:"; echo "-h | --help             : Display this page"; echo "-s | --slow             : Use higher compression on icons, but takes longer"; echo "-b | --build [VERSION]  : Build and prepare the program for release"; echo "-d | --debian [VERSION] : Build the .deb and install"; echo "-v | --version          : Display program version"; echo "-u | --uninstall        : Uninstall the program"; echo "-c | --compress         : Generate and compress icons"; echo "-n | --notifications    : Build the notifications"; echo "-D | --dependencies     : Check if dependencies are installed"; echo ""; echo "--clean                 : Clean up files generated during build"; echo ""; echo "Program written by: Dragon8oy"; exit;;
+  -h|--help) echo "Kernel-notify Copyright (C) 2020 Stuart Hayhurst"; \
+  echo "This program comes with ABSOLUTELY NO WARRANTY."; \
+  echo "This is free software; see the source for copying conditions."; \
+  echo ""; \
+  echo "Usage: ./install.sh [-OPTION]"; \
+  echo "Help:"; \
+  echo "-h | --help             : Display this page"; \
+  echo "-b | --build            : Build the Debian package"; \
+  echo "-d | --debian           : Build and install the program for Debian"; \
+  echo "-i | --install          : Install the program"; \
+  echo "-u | --uninstall        : Uninstall the program"; \
+  echo "-D | --dependencies     : Check if dependencies are installed"; \
+  echo "-x | --update-version   : Update the version of the package"; \
+  echo ""; \
+  echo "Program written by: Dragon8oy"; exit;;
+  -b|--build) checkDeps "b"; make dist; exit;;
+  -i|--install) make build; sudo make install; exit;;
   -u|--uninstall) uninstall; exit;;
-  -n|--notifications) buildNotifications; exit;;
-  -D|--dependencies) checkDeps "pb"; exit;;
-  -c|--compress) compressIcons; exit;;
-  --clean) cleanFiles; exit;;
-  -cs|-sc) slow="true"; compressIcons; exit;;
-  -v|--version) ./kernel-notify -v; exit;;
-  -bs|-sb) slow="true"; checkDeps "b"; buildPackage "$buildVersion"; exit;;
-  -ds|-is|-sd|-si) slow="true"; checkDeps "b"; buildPackage "$buildVersion"; echo "Installing package:"; sudo dpkg -i "kernel-notify-${newVersion}_all.deb"; exit;;
-  -d|-i|--debian|--install) checkDeps "b"; buildPackage "$buildVersion"; echo "Installing package:"; sudo dpkg -i "kernel-notify-${newVersion}_all.deb"; exit;;
-  -b|--build) checkDeps "b"; buildPackage "$buildVersion"; exit;;
+  -d|--debian) installDebian; exit;;
+  -D|--dependencies) checkDeps "pbi"; exit;;
+  -x|--update-version) updateVersion "$buildVersion"; exit;;
+  --make-assist) makeAssist; exit;;
   *) echo "Unknown parameter passed: $1"; exit 1;;
 esac; shift; done
 
 checkDeps "pb"
-installPackage
+exit
+make build; sudo make install;
