3ef80df19c2e82533b9e5acc4b728b1bde5a83a6
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2020-07-25 18:44:20 +0100

    Support when shellcheck version is 0.7.0 or below

--- tests/shellcheck.sh
+++ tests/shellcheck.sh
@@ -3,10 +3,17 @@ while IFS= read -r -d '' i; do
   dirList+="$i/* "
 done <   <(find ./src -type d -print0)
 
+shellcheckVer="$(shellcheck -V |grep version |grep -v license)"
+shellcheckVer="${shellcheckVer/version: }"
+if [[ "$shellcheckVer" > "0.7.0" ]]; then
+  shellcheckArgs=("-W" "0")
+else
+  shellcheckArgs=()
+fi
 for i in $dirList; do
   if [[ -f "$i" ]]; then
     if [[ "$(head -n 1 "$i")" == "#!/bin/bash" ]]; then
-      if ! shellcheck -W 0 "$i"; then
+      if ! shellcheck "${shellcheckArgs[@]}" "$i"; then
         exit 1
       fi
     fi
