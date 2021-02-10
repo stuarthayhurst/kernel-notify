2d0a22c8b6da53c13fdd1d60f962ca61b9b10cc3
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2021-02-10 12:54:08 +0000

    Improved code quality of shellcheck test

--- tests/shellcheck.sh
+++ tests/shellcheck.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 while IFS= read -r -d '' i; do
-  dirList+="$i/* "
+  dirList+=("$i/*")
 done <   <(find ./src -type d -print0)
 
 shellcheckVer="$(shellcheck -V |grep version |grep -v license)"
 shellcheckVer="${shellcheckVer/version: }"
+shellcheckArgs=()
 if [[ "$shellcheckVer" > "0.7.0" ]]; then
-  shellcheckArgs=("-W" "0")
-else
-  shellcheckArgs=()
+  shellcheckArgs+=("-W" "0")
 fi
-for i in $dirList; do
-  if [[ -f "$i" ]]; then
-    if [[ "$(head -n 1 "$i")" == "#!/bin/bash" ]]; then
-      if ! shellcheck "${shellcheckArgs[@]}" "$i"; then
-        exit 1
-      fi
+for files in "${dirList[@]}"; do
+  if [[ -f "$files" ]] && [[ "$(head -n 1 "$files")" == "#!/bin/bash" ]]; then
+    if ! shellcheck "${shellcheckArgs[@]}" "$files"; then
+      exit 1
     fi
   fi
 done
