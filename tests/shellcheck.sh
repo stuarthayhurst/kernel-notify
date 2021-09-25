30ac0e035f94c6ad217757788b7b12b6aff5c253
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2021-09-25 14:41:17 +0100

    Fix shellcheck test

--- tests/shellcheck.sh
+++ tests/shellcheck.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-while IFS= read -r -d '' i; do
-  dirList+=("$i/*")
-done <   <(find ./src -type d -print0)
-
 shellcheckVer="$(shellcheck -V |grep version |grep -v license)"
 shellcheckVer="${shellcheckVer/version: }"
 shellcheckArgs=()
 if [[ "$shellcheckVer" > "0.7.0" ]]; then
   shellcheckArgs+=("-W" "0")
 fi
-for files in "${dirList[@]}"; do
-  if [[ -f "$files" ]] && [[ "$(head -n 1 "$files")" == "#!/bin/bash" ]]; then
-    if ! shellcheck "${shellcheckArgs[@]}" "$files"; then
+
+for file in $(echo */*); do
+  if [[ -f "$file" ]] && grep -q "#!/bin/bash" "$file"; then
+    if ! shellcheck "${shellcheckArgs[@]}" "$file"; then
       exit 1
     fi
   fi
